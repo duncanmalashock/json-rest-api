89d8b7d52bdf4ad2ec1b53d492f58dcc39fef39d
Duncan Malashock <duncanmalashock@gmail.com>
2018-01-29 23:55:46 -0500

    Remove duplication in CustomRequests

--- src/CustomRequests.elm
+++ src/CustomRequests.elm
@@ -5,25 +5,19 @@ import Json.Decode as Decode exposing (Decoder)
 
 
 patchRequest : String -> Http.Body -> Decoder a -> Http.Request a
-patchRequest url body decoder =
-    Http.request
-        { method = "PATCH"
-        , headers =
-            [ Http.header "Accept" "application/json"
-            , Http.header "Content-type" "application/json"
-            ]
-        , url = url
-        , body = body
-        , expect = Http.expectJson decoder
-        , timeout = Nothing
-        , withCredentials = False
-        }
+patchRequest =
+    standardRequest "PATCH"
 
 
 deleteRequest : String -> Http.Body -> Decoder a -> Http.Request a
-deleteRequest url body decoder =
+deleteRequest =
+    standardRequest "DELETE"
+
+
+standardRequest : String -> String -> Http.Body -> Decoder a -> Http.Request a
+standardRequest verb url body decoder =
     Http.request
-        { method = "DELETE"
+        { method = verb
         , headers =
             [ Http.header "Accept" "application/json"
             , Http.header "Content-type" "application/json"
